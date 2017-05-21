be6de8130577534839e4d54f39c2099310868b26
Meghna Gupta <meghna16056@iiitd.ac.in>
2017-05-22 01:17:26 +0530

    Contact.java added

---
+++ app/src/main/java/com/example/saathi/Contact.java
@@ -0,0 +1,17 @@
+package com.example.saathi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HP on 21-05-2017.
+ */
+
+public class Contact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contact);
+    }
+}
